10580292e97b77f67c56c44e62b7f50d4977302d
Dreams0809 <expensivepain9@gmail.com>
2023-11-16 23:58:36 -0500

    index.html

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
-        store: new MongoStore.create({  mongoUrl: process.env.DB_STRING})
+        store: new MongoStore({mongooseConnection: mongoose.connection})
     })
 )
 
